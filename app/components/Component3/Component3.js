5c89ada1d036fe815c1ff4096370afc9778a743f
Ramjan Ali <me.ramjanali@gmail.com>
2017-08-26 04:37:04 +0600

    navigator uses

---
+++ app/components/Component3/Component3.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { 
+    AppRegistry, 
+    Text, 
+    View,
+    Button
+} from 'react-native';
+
+export default class Component3 extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: this.props.user.name,
+            email: this.props.user.email
+        }
+    }
+    onPress(){
+        this.props.navigator.push({
+            id: 'component2'
+        });
+    }
+    render() {
+        return (
+            <View>
+                <Text>{this.state.name}</Text>
+                <Text>{this.state.email}</Text>
+                <Button 
+                    onPress={this.onPress.bind(this)}
+                    title="Go Back"
+                />
+            </View>
+        );
+    }
+}
+
+AppRegistry.registerComponent('Component3', () => Component3);
--- index.android.js
+++ index.android.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View } from 'react-native';
+import { Navigator } from 'react-native-deprecated-custom-components';
 
-import Component1 from './app/components/Component1/Component1'
+import Component2 from './app/components/Component2/Component2';
+import Component3 from './app/components/Component3/Component3';
 
 export default class myapp extends Component {
+    renderScene(route, navigator){
+        switch(route.id){
+            case 'component2': 
+                return (<Component2 navigator={navigator} title="Component2" />);
+            case 'component3': 
+                return (<Component3 user={route.user} navigator={navigator} title="Component3" />);
+        }
+    }
     render() {
         return ( 
-            <View >
-                <Component1 / >
-            </View>
+            <Navigator 
+                initialRoute={{id: 'component2'}}
+                renderScene={this.renderScene}
+                configureScreen={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
+            />
         );
     }
 }
